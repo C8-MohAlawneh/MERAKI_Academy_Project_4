1b56a0c0a3f6ccee1961ba6d41c1184ea0ef5df8
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-08-22 23:44:28 +0300

    FrontEnd delete post

--- frontend/src/components/Home/Posts/index.js
+++ frontend/src/components/Home/Posts/index.js
@@ -26,91 +26,117 @@ const Posts = () => {
         });
     };
     getAllPosts();
-    console.log(posts);
+    // console.log(posts);
   }, []);
   return (
-    <div className="posts-container">
-      {posts.map((post) => {
-        return (
-          <div key={post._id} className="one-post-container">
-            {/* the info of user who post this post */}
-            <div className="poster-info">
-              <img
-                className="post-image"
-                src={post.user.userPhoto}
-                width={50}
-                height={50}
-              />
-              <h3>{`${post.user.firstName} ${post.user.lastName}`}</h3>
-            </div>
-            {/* start post content and update */}
-            <div className="post-content">
-              {updateBtn && updateId === post._id ? (
-                <>
-                  <input
-                    defaultValue={post.post}
-                    onChange={(e) => {
-                      setNewUpdateOfPost({ post: e.target.value });
-                      console.log(newUpdateOfPost);
-                    }}
+    <>
+      <div className="create-new-post-container">
+        <input />
+        <button>Post</button>
+      </div>
+      <div className="posts-container">
+        {posts &&
+          posts.map((post) => {
+            return (
+              <div key={post._id} className="one-post-container">
+                {/* the info of user who post this post */}
+                <div className="poster-info">
+                  <img
+                    className="post-image"
+                    src={post.user.userPhoto}
+                    width={50}
+                    height={50}
                   />
-                  {
-                    <BiSolidCheckCircle
-                      onClick={() => {
-                        axios
-                          .put(
-                            `http://localhost:5000/posts/${post._id}`,
-                            newUpdateOfPost
-                          )
-                          .then((res) => {
-                            // this function to rerender the updated posts
-                            setPosts(
-                              posts.map((elem) => {
-                                if (elem._id === post._id) {
-                                  elem.post = newUpdateOfPost.post;
-                                  return elem;
-                                }
-                                return elem;
+                  <h3>{`${post.user.firstName} ${post.user.lastName}`}</h3>
+                </div>
+                {/* start post content and update */}
+                <div className="post-content">
+                  {updateBtn && updateId === post._id ? (
+                    <>
+                      <input
+                        defaultValue={post.post}
+                        onChange={(e) => {
+                          setNewUpdateOfPost({ post: e.target.value });
+                        }}
+                      />
+                      {
+                        <BiSolidCheckCircle
+                          onClick={() => {
+                            axios
+                              .put(
+                                `http://localhost:5000/posts/${post._id}`,
+                                newUpdateOfPost
+                              )
+                              .then((res) => {
+                                // this function to rerender the updated posts
+                                setPosts(
+                                  posts.map((elem) => {
+                                    if (elem._id === post._id) {
+                                      elem.post = newUpdateOfPost.post;
+                                    }
+                                    return elem;
+                                  })
+                                );
+                                setUpdateBtn(false);
                               })
-                            );
-                            setUpdateBtn(false);
-                          })
-                          .catch((err) => {
-                            console.log(err);
+                              .catch((err) => {
+                                console.log(err);
+                              });
+                          }}
+                        />
+                      }
+                    </>
+                  ) : (
+                    <h4>{post.post}</h4>
+                  )}
+                  {/* this element show the update and delete buttons for the login user*/}
+                  {post.user._id === userId && (
+                    <>
+                      <button
+                        onClick={() => {
+                          setUpdateId(post._id);
+                          setUpdateBtn((prv) => {
+                            return !prv;
                           });
-                      }}
-                    />
-                  }
-                </>
-              ) : (
-                <h4>{post.post}</h4>
-              )}
-              {post.user._id === userId && (
-                <button
-                  onClick={() => {
-                    setUpdateId(post._id);
-                    setUpdateBtn((prv) => {
-                      return !prv;
-                    });
-                  }}
-                >
-                  update
-                </button>
-              )}
-            </div>
-            <div className="comment-content">
-              {post.comments.map((comment) => {
-                return (
-                  <div key={comment._id} className="comment-container">
-                    <p>{comment.comment}</p>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        );
-      })}
-    </div>
+                        }}
+                      >
+                        update
+                      </button>
+                      <button
+                        onClick={() => {
+                          axios
+                            .delete(`http://localhost:5000/posts/${post._id}`)
+                            .then(() => {
+                              setPosts(
+                                posts.filter((elem) => {
+                                  return elem._id !== post._id;
+                                })
+                              );
+                            })
+                            .catch((err) => {
+                              console.log(err);
+                            });
+                        }}
+                      >
+                        delete
+                      </button>
+                    </>
+                  )}
+                </div>
+                <div className="comment-content">
+                  {post.comments.map((comment) => {
+                    return (
+                      <div key={comment._id} className="comment-container">
+                        <p>{comment.comment}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </>
   );
 };
 
