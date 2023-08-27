74ef7a29381bc13643f3e36d217d4cc804db87be
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-08-27 21:00:11 +0300

    FrontEnd Accept and delete request of friends

--- frontend/src/components/AddFriends/index.js
+++ frontend/src/components/AddFriends/index.js
@@ -9,18 +9,31 @@ const AddFriends = () => {
   const [allProfiles, setAllProfiles] = useState([]);
   const [initLoading, setInitLoading] = useState(true);
   const [loading, setLoading] = useState(false);
+  const [friendsReq, setFriendsReq] = useState([]);
 
   useEffect(() => {
     friendProfiles();
+    getMyProfile();
   }, []);
+  const getMyProfile = () => {
+    axios
+      .get("http://localhost:5000/users/profile", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((result) => {
+        console.log(result.data.profile);
+        setFriendsReq(result.data.profile.friendsReq);
+      })
+      .catch();
+  };
   const friendProfiles = () => {
     axios
       .get("http://localhost:5000/users//allProfile", {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((users) => {
-        // console.log(users.data.profile);
         setAllProfiles(users.data.profile);
+
         setInitLoading(false);
       })
       .catch((err) => {
@@ -42,7 +55,88 @@ const AddFriends = () => {
     ) : null;
   return (
     <div>
-      <div className="friend-request-container"></div>
+      <div className="friend-request-container">
+        <List
+          header={<h3 className="header-of-list">Friends Request</h3>}
+          style={{ width: "60%" }}
+          size="small"
+          bordered
+          className="demo-loadmore-list"
+          loading={initLoading}
+          itemLayout="horizontal"
+          dataSource={friendsReq}
+          renderItem={(item) => (
+            <List.Item
+              actions={[
+                <Button
+                  type="primary"
+                  style={{ backgroundColor: "green" }}
+                  onClick={async () => {
+                    try {
+                      await axios.put(
+                        `http://localhost:5000/users/accept/${item._id}`,
+                        "",
+                        {
+                          headers: {
+                            Authorization: `Bearer ${token}`,
+                          },
+                        }
+                      );
+                      setFriendsReq(
+                        friendsReq.filter((elem) => {
+                          return elem._id !== item._id;
+                        })
+                      );
+                    } catch (err) {
+                      console.log(err);
+                    }
+                  }}
+                >
+                  Accept
+                </Button>,
+                <Button
+                  type="primary"
+                  style={{ backgroundColor: "red" }}
+                  onClick={async () => {
+                    try {
+                      await axios.put(
+                        `http://localhost:5000/users/delete/${item._id}`,
+                        "",
+                        {
+                          headers: {
+                            Authorization: `Bearer ${token}`,
+                          },
+                        }
+                      );
+                      setFriendsReq(
+                        friendsReq.filter((elem) => {
+                          return elem._id !== item._id;
+                        })
+                      );
+                    } catch (err) {
+                      console.log(err);
+                    }
+                  }}
+                >
+                  Delete
+                </Button>,
+              ]}
+            >
+              <Skeleton avatar title={false} loading={item.loading} active>
+                <List.Item.Meta
+                  avatar={<Avatar src={item.userPhoto} />}
+                  title={
+                    <a href="https://ant.design">
+                      {item.firstName + " " + item.lastName}
+                    </a>
+                  }
+                  description={item.email}
+                />
+              </Skeleton>
+            </List.Item>
+          )}
+        />
+      </div>
       <div className="all-profile-container">
         <List
           header={<h3 className="header-of-list">Discover New Friends</h3>}
@@ -63,7 +157,7 @@ const AddFriends = () => {
                     axios
                       .put(
                         `http://localhost:5000/users/sendFriendReq/${item._id}`,
-                        allProfiles,
+                        "",
                         {
                           headers: {
                             Authorization: `Bearer ${token}`,
@@ -71,12 +165,13 @@ const AddFriends = () => {
                         }
                       )
                       .then((result) => {
-                        console.log(result);
-                        setAllProfiles(
-                          allProfiles.filter((profile) => {
-                            return profile._id !== item._id;
-                          })
-                        );
+                        setTimeout(() => {
+                          setAllProfiles(
+                            allProfiles.filter((profile) => {
+                              return profile._id !== item._id;
+                            })
+                          );
+                        }, 500);
                       })
                       .catch((err) => {
                         console.log(err);
@@ -90,7 +185,11 @@ const AddFriends = () => {
               <Skeleton avatar title={false} loading={item.loading} active>
                 <List.Item.Meta
                   avatar={<Avatar src={item.userPhoto} />}
-                  title={<a href="https://ant.design">{item.firstName}</a>}
+                  title={
+                    <a href="https://ant.design">
+                      {item.firstName + " " + item.lastName}
+                    </a>
+                  }
                   description={item.email}
                 />
               </Skeleton>
