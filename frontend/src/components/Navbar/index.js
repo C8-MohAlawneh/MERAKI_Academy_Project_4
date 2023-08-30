59d4c58f2ed09ba3d486248f9479d00280a6afff
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-08-30 22:46:49 +0300

    desiging home page

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
-import Logo from "./Logo.png";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../../AppContext";
 import { UsergroupAddOutlined, LogoutOutlined } from "@ant-design/icons";
-import { Avatar, Input } from "antd";
+import { Avatar, Input, Layout } from "antd";
 import axios from "axios";
+
+const { Header } = Layout;
 const { Search } = Input;
 
 const Navbar = () => {
@@ -29,59 +30,68 @@ const Navbar = () => {
       .then((result) => {
         setProfile(result.data.profile);
       })
-      .catch();
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
-    <div className="home-nav-bar">
-      <img
-        src={Logo}
-        width={100}
-        height={50}
-        onClick={() => {
-          navigate("/home");
-        }}
-      />
-      <Search
-        placeholder="Search"
-        enterButton
-        style={{ width: "25%" }}
-        onChange={(e) => {
-          setSearchByName(e.target.value);
-        }}
-        onPressEnter={() => {
-          axios
-            .post(`http://localhost:5000/users/search/${searchByName}`)
-            .then((result) => {
-              setAllProfiles(result.data.result);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        }}
-      />
-      <Avatar
-        src={profile.userPhoto}
-        onClick={() => {
-          navigate("/profile");
-        }}
-      />
-      <UsergroupAddOutlined
-        style={{ color: "blue" }}
-        onClick={() => {
-          navigate("/addFriends");
-        }}
-      />
-      <LogoutOutlined
-        style={{ color: "red" }}
-        onClick={() => {
-          localStorage.clear();
-          setToken(null);
-          setIsLoggedIn(false);
-          navigate("/login");
-          setProfile({});
-        }}
-      />
-    </div>
+    <Layout>
+      <Header>
+        <div className="home-nav-bar">
+          <h2
+            style={{ color: "white", cursor: "pointer" }}
+            onClick={() => {
+              navigate("/home");
+            }}
+          >
+            Social Media PlatForm
+          </h2>
+          <Search
+            placeholder="Search"
+            enterButton
+            style={{ width: "25%" }}
+            onChange={(e) => {
+              setSearchByName(e.target.value);
+            }}
+            onPressEnter={() => {
+              axios
+                .post(`http://localhost:5000/users/search/${searchByName}`)
+                .then((result) => {
+                  setAllProfiles(result.data.result);
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }}
+          />
+          <Avatar
+            style={{ cursor: "pointer" }}
+            src={profile.userPhoto}
+            onClick={() => {
+              navigate("/profile");
+            }}
+          />
+          <UsergroupAddOutlined
+            style={{ color: "blue", fontSize: "150%" }}
+            onClick={() => {
+              navigate("/addFriends");
+            }}
+          />
+          <LogoutOutlined
+            style={{ color: "red", fontSize: "150%" }}
+            onClick={() => {
+              localStorage.clear();
+              setToken(null);
+              setIsLoggedIn(false);
+              navigate("/login");
+              setProfile({});
+            }}
+          />
+        </div>
+        <div className="demo-logo" />
+        {/* <Menu theme="dark" mode="horizontal" /> */}
+      </Header>
+    </Layout>
   );
 };
 
